69e704122b70f7ed73710213925e7e2053d760b4
JoseRibelles <joseantonioribellesgarcia@elpuig.xeill.net>
2026-04-21 11:45:53 +0200

    Remove CorsConfig class for CORS configuration

--- src/main/java/com/backend/apirest/CorsConfig.java
+++ src/main/java/com/backend/apirest/CorsConfig.java
@@ -1,17 +1,1 @@
-package com.backend.apirest;
-
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                    .allowedOrigins("https://conflict-front-end-jose-ribelles.vercel.app")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowCredentials(true);
-            }
-        };
-    }
-}
+##
